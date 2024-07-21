9cb2de52088c87d7549d32a87165db074ea7699c
jonasdataloop <jonas.m@dataloop.ai>
2024-07-21 13:28:23 +0300

    cleanup

---
+++ README.md
@@ -0,0 +1,45 @@
+# Vertex AI Model Adapters
+
+Welcome to the repository for Dataloop model adapters that utilize Google Vertex AI models. Follow the instructions below to set up and use these adapters effectively.
+
+For more information on Vertex AI models, refer to the [official documentation](https://cloud.google.com/vertex-ai/generative-ai/docs/learn/models).
+
+## Supported Models
+
+- **Gemini 1.5 Pro**
+- **PaLM 2 Chat Bison**
+
+## Setting Up Your GCP Project
+
+To use these models, you need a Google Cloud Platform (GCP) project. Follow these steps to get started:
+
+### 1. Enable the Vertex AI API
+   - Navigate to the API Library in the GCP Console.
+   - Enable the Vertex AI API.
+
+### 2. Create a [Service Account](https://docs.dataloop.ai/docs/private-key-integration?highlight=create%20service%20account)
+   - Go to the IAM & Admin section in the GCP Console.
+   - Create a new service account.
+   - Generate a new key and download the service account JSON file.
+
+### 3. Assign Permissions
+   - Grant the service account the `aiplatform.endpoints.predict` permission.
+
+## Integrating Google Vertex AI with Dataloop Platform
+
+### 1. Install the Model
+   - Visit the [Dataloop Marketplace](https://docs.dataloop.ai/docs/marketplace).
+   - Install the desired model.
+
+### 2. Create the Integration Service Account
+   - Go to the [Data Governance](https://docs.dataloop.ai/docs/overview-1?highlight=data%20governance) section in the Dataloop platform.
+   - Click on "Create Integration".
+   - Choose an integration name, select "GCP" as the provider, and "Private Key" as the integration type.
+   - Import the service account JSON file you previously downloaded.
+
+### 3. Configure the Service in Data Governance
+   - Add the integration to the model's [service configuration](https://docs.dataloop.ai/docs/service-runtime#secrets-for-faas) by specifying the integration name you created.
+
+---
+
+For additional assistance or inquiries, please refer to the Dataloop documentation or contact support.
---
+++ adapters/bison/chat_bison.py
@@ -0,0 +1,94 @@
+import os
+import base64
+import json
+import dtlpy as dl
+import logging
+from vertexai.language_models import ChatModel
+from google.cloud import storage
+
+logger = logging.getLogger("Vertex AI Adapter")
+
+
+class ModelAdapter(dl.BaseModelAdapter):
+    def __init__(self, model_entity: dl.Model, integration_name):
+        super().__init__(model_entity)
+
+        self.max_token = self.model_entity.configuration.get('max_token', 1024)
+        self.temperature = self.model_entity.configuration.get('temperature', 0.2)
+        self.top_p = self.model_entity.configuration.get('top_p', 0.7)
+        self.top_k = self.model_entity.configuration.get('top_k', 40)
+        self.context = self.model_entity.configuration.get('system_prompt', '')
+        self.model_name = self.model_entity.configuration.get('model_name', 'chat-bison@002')
+
+        credentials = os.environ.get(integration_name.replace('-', '_'))
+
+        # for case of integration
+        credentials = base64.b64decode(credentials)
+        credentials = credentials.decode("utf-8")
+        credentials = json.loads(credentials)
+        credentials = json.loads(credentials['content'])
+        self.client = storage.Client.from_service_account_info(info=credentials)
+
+    def load(self, local_path, **kwargs):
+        pass
+
+    def prepare_item_func(self, item: dl.Item):
+        if 'json' not in item.mimetype or item.metadata.get('system', dict()).get('shebang', dict()).get(
+                'dltype') != 'prompt':
+            logger.warning(f"Item is not a JSON file or a Prompt item.")
+            return None
+        buffer = json.load(item.download(save_locally=False))
+        return buffer
+
+    def predict(self, batch, **kwargs):
+        annotations = []
+        for prompt_item in batch:
+            ann_collection = dl.AnnotationCollection()
+            for prompt_name, prompt_content in prompt_item.get('prompts').items():
+                text = None
+                context = None
+                for partial_prompt in prompt_content:
+                    if 'text' in partial_prompt.get('mimetype', ''):
+                        text = partial_prompt.get('value')
+                    else:
+                        logger.warning(
+                            f"Prompt Type: {partial_prompt.get('mimetype', '')}. Only text prompt from type is "
+                            f"supported.")
+                if text is None:
+                    logger.warning(f"{prompt_name} is missing a text prompt.")
+                    continue
+
+                parameters = {
+                    "temperature": self.temperature,
+                    "max_output_tokens": self.max_token,
+                    "top_p": self.top_p,
+                    "top_k": self.top_k
+                }
+
+                chat_model = ChatModel.from_pretrained(self.model_name)
+                chat = chat_model.start_chat(
+                    context=self.context if self.context else None,
+                )
+                response = chat.send_message(
+                    text, **parameters
+                )
+
+                content = response.text
+                ann_collection.add(
+                    annotation_definition=dl.FreeText(text=content),
+                    prompt_id=prompt_name,
+                    model_info={
+                        'name': self.model_entity.name,
+                        'model_id': self.model_entity.id,
+                        'confidence': 1.0
+                    }
+                )
+            annotations.append(ann_collection)
+        return annotations
+
+
+if __name__ == '__main__':
+    model = dl.models.get(model_id='')
+    item = dl.items.get(item_id='')
+    adapter = ModelAdapter(model)
+    adapter.predict_items(items=[item])
